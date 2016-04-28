e564cf4585a19b3090a511091455d1dc6a44a832
Álvaro Fernández García <alvfergar@gmail.com>
2016-04-28 12:58:45 +0200

    Gradle updated and changes in layout list item forecast to adapt new design

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -59,13 +59,13 @@ public class ForecastAdapter extends CursorAdapter {
     /*
         This is where we fill-in the views with the contents of the cursor.
      */
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
-
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
-    }
+//    @Override
+//    public void bindView(View view, Context context, Cursor cursor) {
+//        // our view is pretty simple here --- just a text view
+//        // we'll keep the UI functional with a simple (and slow!) binding.
+//
+//        TextView tv = (TextView)view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
+//    }
 
 }
